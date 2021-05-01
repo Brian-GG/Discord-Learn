74b5ef88cf3f9c5bd9a3e07d832bd676905fbd2f
Brian Cho <h9113847@gmail.com>
2021-05-01 00:12:29 -0400

    added some breakout

--- breakout.py
+++ breakout.py
@@ -5,10 +5,22 @@ class Breakout(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
-    async def test(self, ctx):
-        await ctx.send('this worked!')
+    @commands.command(name='create-breakout')
+    async def create_breakout(self, ctx, arg):
 
+        guild = ctx.guild
 
+        category = await guild.create_category('Breakout Rooms')
+
+        if ctx.author.voice and ctx.author.voice.channel:
+            channel = ctx.author.voice.channel
+        else:
+            await ctx.send("You are not connected to a voice channel")
+            return
+
+        students = []
+        
+
+        breakout_room = await guild.create_voice_channel('Breakout Room 1', category=category)
 
 
